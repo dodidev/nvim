15f7ee2a09500c041f95c560b2886bd6a1adfee6
dev07 <me@dodi.dev>
2025-04-08 10:31:21 +0700

    Add keymap open terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,10 @@
 -- Add any additional keymaps here
 
 local opts = { noremap = true, silent = true }
-
+-- Auto fix
 vim.api.nvim_set_keymap("n", "<leader>af", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 vim.api.nvim_set_keymap("v", "<leader>af", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
+
+-- Terminal
+vim.api.nvim_set_keymap("n", "<leader>tt", ":split | terminal<CR>", { desc = "Open terminal in split" })
+vim.api.nvim_set_keymap("n", "<leader>tv", ":vsplit | terminal<CR>", { desc = "Open terminal in vsplit" })
